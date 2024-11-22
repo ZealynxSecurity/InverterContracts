7e6d10be2d71dceb2299a89e04276e4d52749486
vendrell46 <vendrell_46@hotmail.com>
2024-11-22 20:46:53 +0100

    extended IRedeemingBondingCurveBase_v1 as well

--- src/modules/fundingManager/oracle/interfaces/IFM_PC_Oracle_Redeeming_v1.sol
+++ src/modules/fundingManager/oracle/interfaces/IFM_PC_Oracle_Redeeming_v1.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.23;
 
 import { IFundingManager_v1 } from "@fm/IFundingManager_v1.sol";
 import { IERC20PaymentClientBase_v1 } from "@lm/interfaces/IERC20PaymentClientBase_v1.sol";
+import { IRedeemingBondingCurveBase_v1 } from "@fm/bondingCurve/interfaces/IRedeemingBondingCurveBase_v1.sol";
 
 /**
  * @title   Oracle Price Funding Manager Interface
@@ -12,7 +13,8 @@ import { IERC20PaymentClientBase_v1 } from "@lm/interfaces/IERC20PaymentClientBa
  */
 interface IFM_PC_Oracle_Redeeming_v1 is
     IFundingManager_v1,
-    IERC20PaymentClientBase_v1 
+    IERC20PaymentClientBase_v1,
+    IRedeemingBondingCurveBase_v1
 {
 
     function buy(uint256 collateralAmount) external returns (uint256 tokenAmount);

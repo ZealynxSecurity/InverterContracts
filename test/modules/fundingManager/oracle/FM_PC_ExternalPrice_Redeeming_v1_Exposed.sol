6d14179978ae5b9dac39ef9ab71089d76e632158
Zitzak <marvin88gr@gmail.com>
2025-01-09 17:12:38 +0100

    refactor: FM_PC_ExternalPrice_Redeeming_v1_Test
    - Remove imports
    - Refactor most of the setup
    - Add exposed mock
    - Removed OraclePriceMock and InvalidMock
    - Added simpler oracle mockf
    - fmt

---
+++ test/modules/fundingManager/oracle/FM_PC_ExternalPrice_Redeeming_v1_Exposed.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.23;
+
+import {FM_PC_ExternalPrice_Redeeming_v1} from
+    "src/modules/fundingManager/oracle/FM_PC_ExternalPrice_Redeeming_v1.sol";
+
+contract FM_PC_ExternalPrice_Redeeming_v1_Exposed is
+    FM_PC_ExternalPrice_Redeeming_v1
+{}

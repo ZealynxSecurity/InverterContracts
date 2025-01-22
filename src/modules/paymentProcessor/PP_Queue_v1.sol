ed614ff36d7bc081bbeaab96d9c0ae720c612937
scab24 <git.seco@protonmail.com>
2025-01-22 11:43:31 +0100

    fix (;)

--- src/modules/paymentProcessor/PP_Queue_v1.sol
+++ src/modules/paymentProcessor/PP_Queue_v1.sol
@@ -13,7 +13,7 @@ import {LinkedIdList} from "src/modules/lib/LinkedIdList.sol";
 
 // External
 import {IERC20} from "@oz/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@oz/token/ERC20/utils/SafeERC20.sol";;
+import {SafeERC20} from "@oz/token/ERC20/utils/SafeERC20.sol";
 import {IERC20Metadata} from "@oz/token/ERC20/extensions/IERC20Metadata.sol";
 
 /**
